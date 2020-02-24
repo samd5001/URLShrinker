f2fdd5be78cb66100b0ebee4916014b8edd45203
Sam Dunkley <samd5002@gmail.com>
2020-02-24 20:41:17 +0000

    Initial project from Spring Initialzr

---
+++ src/main/java/com/samdunkley/urlshrinker/UrlshrinkerApplication.java
@@ -0,0 +1,13 @@
+package com.samdunkley.urlshrinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshrinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshrinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samdunkley/urlshrinker/UrlshrinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samdunkley.urlshrinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshrinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
